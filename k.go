9c5ebbf1c8b5a6375ee0215375e9f60282946b0c
codeudan <fyyz31311@gmail.com>
2019-05-11 17:55:18 +0800

    <update> add colors and refactor

--- k.go
+++ k.go
@@ -8,40 +8,38 @@ import (
 	"regexp"
 	"runtime"
 	"strings"
+	"text/template"
 
 	"github.com/manifoldco/promptui"
 )
 
+const size int = 10
+const portMaxDisplay int = 5
+
 func main() {
 	if runtime.GOOS == "windows" {
+		fmt.Println("Windows is not supported")
 		os.Exit(1)
 	}
 
 	pidToPorts := listPortByPid()
 	psList := listProcess(pidToPorts)
-	psDisplayList := []string{}
-	for _, ps := range psList {
-		str := ""
-		str = str + ps.Name + " " + "(" + ps.Pid + ")"
-		if len(ps.Ports) > 3 {
-			port1 := ps.Ports[0]
-			portEnd := ps.Ports[len(ps.Ports)-1]
-			str = str + " " + port1 + " ... " + portEnd
-		} else {
-			str = str + " " + strings.Join(ps.Ports, ", ")
-		}
-		psDisplayList = append(psDisplayList, str)
-	}
 	prompt := promptui.Select{
 		Label:             "",
-		Items:             psDisplayList,
-		Size:              10,
+		Items:             psList,
+		Size:              size,
 		StartInSearchMode: true,
 		Templates: &promptui.SelectTemplates{
 			Label:    "Running processes:",
-			Active:   "❯ {{ . }}",
-			Inactive: "  {{ . }}",
-			Selected: "  {{ . }}",
+			Active:   `{{"❯ " | cyan }}{{.Name | cyan}} {{ "(" | cyan}}{{ .Pid | cyan }}{{")" | cyan}} {{ .PortsStr | redLight}}`,
+			Inactive: `  {{.Name }} ({{ .Pid }}) {{ .PortsStr | red}}`,
+			Selected: `  {{.Name }} ({{ .Pid }}) {{ .PortsStr | red}}`,
+			FuncMap: template.FuncMap{
+				"faint":    promptui.Styler(0, 0, 31),
+				"cyan":     promptui.Styler(0, 0, 36),
+				"red":      promptui.Styler(2, 40, 31),
+				"redLight": promptui.Styler(0, 0, 31),
+			},
 		},
 		Searcher: func(input string, idx int) bool {
 			p := psList[idx]
@@ -65,21 +63,22 @@ func main() {
 	idx, _, err := prompt.Run()
 	if err == nil {
 		kill(psList[idx].Pid)
+		return
 	}
 }
 
 type ps struct {
-	Pid   string
-	Comm  string
-	Name  string
-	Ports []string
+	Pid      string
+	Name     string
+	Ports    []string
+	PortsStr string
 }
 
 func listProcess(pidToPorts map[string][]string) []ps {
 	cmd := exec.Command("ps", "wwxo", "pid,comm")
 	stdout, err := cmd.Output()
 	if err != nil {
-		os.Exit(1)
+		errorHandler(err)
 	}
 	result := strings.Split(strings.TrimSpace(string(stdout)), "\n")
 	result = result[1:]
@@ -88,6 +87,15 @@ func listProcess(pidToPorts map[string][]string) []ps {
 		line = strings.TrimSpace(line)
 		pid := strings.SplitN(line, " ", 2)[0]
 		comm := line[len(pid)+1:]
+		ports := pidToPorts[pid]
+		portsStr := ""
+		if len(ports) > portMaxDisplay {
+			portStart := ports[0]
+			portEnd := ports[len(ports)-1]
+			portsStr = portsStr + " " + portStart + " ··· " + portEnd
+		} else {
+			portsStr = portsStr + " " + strings.Join(ports, ", ")
+		}
 		if strings.HasSuffix(comm, "-helper") ||
 			strings.HasSuffix(comm, "Helper") ||
 			strings.HasSuffix(comm, "HelperApp") {
@@ -95,42 +103,43 @@ func listProcess(pidToPorts map[string][]string) []ps {
 		}
 		data = append(data, ps{
 			pid,
-			comm,
 			filepath.Base(comm),
 			pidToPorts[pid],
+			portsStr,
 		})
 	}
 	return data
 }
 
 func listPortByPid() map[string][]string {
+	validNetLineRegStr := "^\\s*(tcp|udp)"
+	allFieldsReg, _ := regexp.Compile("([\\w*.*])+")
+	portReg, _ := regexp.Compile("[^]*[.:](\\d+)$")
+
 	osName := runtime.GOOS
 	pidToPorts := map[string][]string{}
 	cmdOutput := getNetstatOutput()
 	lists := strings.Split(strings.TrimSpace(cmdOutput), "\n")
-	validLineRegStr := "^\\s*(tcp|udp)"
-	itemReg, _ := regexp.Compile("([\\w*.*])+")
-	portReg, _ := regexp.Compile("[^]*[.:](\\d+)$")
 	for _, line := range lists {
-		isValidLine, _ := regexp.MatchString(validLineRegStr, line)
+		isValidLine, _ := regexp.MatchString(validNetLineRegStr, line)
 		if !isValidLine {
 			continue
 		}
-		lineData := itemReg.FindAllString(line, -1)
-		if osName == "darwin" && len(lineData) < 10 {
-			end := append([]string{}, lineData[5:]...)
-			start := append(lineData[0:5], "")
-			lineData = append(start, end...)
+		allFields := allFieldsReg.FindAllString(line, -1)
+		if osName == "darwin" && len(allFields) < 10 {
+			end := append([]string{}, allFields[5:]...)
+			start := append(allFields[0:5], "")
+			allFields = append(start, end...)
 		}
 		portData := []string{}
 		pid := ""
 		if osName == "darwin" {
-			portData = portReg.FindAllString(lineData[3], -1)
-			pid = lineData[8]
+			portData = portReg.FindAllString(allFields[3], -1)
+			pid = allFields[8]
 		}
 		if osName == "linux" {
-			portData = portReg.FindAllString(lineData[4], -1)
-			pid = lineData[6]
+			portData = portReg.FindAllString(allFields[4], -1)
+			pid = allFields[6]
 		}
 		if pidToPorts[pid] == nil {
 			pidToPorts[pid] = []string{}
@@ -152,12 +161,12 @@ func getNetstatOutput() string {
 		tcpCmd := exec.Command("netstat", "-anv", "-p", "tcp")
 		tcp, tcpErr := tcpCmd.Output()
 		if tcpErr != nil {
-			os.Exit(1)
+			errorHandler(tcpErr)
 		}
 		udpCmd := exec.Command("netstat", "-anv", "-p", "udp")
 		udp, udpErr := udpCmd.Output()
 		if udpErr != nil {
-			os.Exit(1)
+			errorHandler(udpErr)
 		}
 		listStr = string(tcp) + "\n" + string(udp)
 	}
@@ -165,7 +174,7 @@ func getNetstatOutput() string {
 		cmd := exec.Command("ss", "-tunlp")
 		out, outErr := cmd.Output()
 		if outErr != nil {
-			os.Exit(1)
+			errorHandler(outErr)
 		}
 		listStr = string(out)
 	}
@@ -176,20 +185,18 @@ func kill(pid string) {
 	killCmd := exec.Command("kill", pid)
 	_, killErr := killCmd.Output()
 	if killErr == nil {
-		fmt.Println("  Killed " + pid)
+		fmt.Println("  Killed ")
 		return
 	}
-
 	prompt := promptui.Prompt{
 		Label:     "Force kill?",
 		IsConfirm: true,
 	}
-
 	confirm, _ := prompt.Run()
 	if confirm == "y" {
 		killCmd := exec.Command("kill", "-9", pid)
 		killCmd.Output()
-		fmt.Println("  Killed " + pid)
+		fmt.Println("  Killed ")
 	}
 }
 
@@ -200,4 +207,9 @@ func contains(list []string, s string) bool {
 		}
 	}
 	return false
+}
+
+func errorHandler(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
